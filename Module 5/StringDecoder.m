6fdf4023f2041559cf79acfefdbea1ba17a11517
mstotter <mstotter@users.noreply.github.com>
2019-04-08 18:39:55 -0400

    Finished string decoder

--- Module 5/StringDecoder.m
+++ Module 5/StringDecoder.m
@@ -1,4 +1,10 @@
 in_message_string = 'potato';
-in_message_binary = reshape(logical(dec2bin(in_message_string) - 48).',1,[])
+in_message_binary = reshape(logical(dec2bin(in_message_string) - 48).',1,[]);
+display(in_message_binary);
+
+
 out_message_binary = in_message_binary;
-%out_message_string
+
+
+out_message_string = char(bin2dec(char(reshape(out_message_binary,7,[]).' + 48))).';
+display(out_message_string);
