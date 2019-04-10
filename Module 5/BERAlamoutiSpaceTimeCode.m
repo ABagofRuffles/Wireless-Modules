e9929af165939bb097af39e86765da023cd7d327
Rafael Rosales <rafaelr1995@gmail.com>
2019-04-09 23:22:22 -0400

    renamed variables

--- Module 5/BERAlamoutiSpaceTimeCode.m
+++ Module 5/BERAlamoutiSpaceTimeCode.m
@@ -12,7 +12,7 @@ NumberofBits = 10^6; % number of bits or symbols
 Eb_N0_dB = [0:25]; % multiple Eb/N0 values
 
 
-for ii = 1:length(Eb_N0_dB)
+for index = 1:length(Eb_N0_dB)
     % Transmitter
     ip = rand(2,NumberofBits) > 0.5; % generating 0,1 with equal probability
     currentBit = 2*ip-1; % BPSK modulation 0 -> -1; 1 -> 0
@@ -22,21 +22,21 @@ for ii = 1:length(Eb_N0_dB)
     sCode(:,1:2:end) = (1/sqrt(2))*reshape(currentBit,4,NumberofBits/2); % [x1 x2  ...]
     sCode(:,2:2:end) = (1/sqrt(2))*(kron(ones(2,NumberofBits/2),[-1;1]).*flipud(reshape(conj(currentBit),4,NumberofBits/2))); % [-x2* x1* ....]
 
-    h = 1/sqrt(2)*[randn(2,NumberofBits) + 1i*randn(2,NumberofBits)]; % Rayleigh channel
-    hMod = kron(reshape(h,4,NumberofBits/2),ones(1,2)); % repeating the same channel for two symbols
-    n = 1/sqrt(2)*[randn(4,NumberofBits) + 1i*randn(4,NumberofBits)]; % white gaussian noise, 0dB variance
+    rayleigh = 1/sqrt(2)*[randn(4,NumberofBits) + 1i*randn(4,NumberofBits)]; % Rayleigh channel
+    hMod = kron(reshape(rayleigh,4,NumberofBits/2),ones(1,4)); % repeating the same channel for two symbols
+    whiteGNoise = 1/sqrt(2)*[randn(4,NumberofBits) + 1i*randn(4,NumberofBits)]; % white gaussian noise, 0dB variance
 
     % Channel and noise Noise addition
-    y = sum(hMod.*sCode,2) + 10^(-Eb_N0_dB(ii)/20)*n;
+    signal = sum(hMod.*sCode,2) + 10^(-Eb_N0_dB(index)/20)*whiteGNoise;
 
     % Receiver
-    yMod = kron(reshape(y,4,NumberofBits/2),ones(1,2)); % [y1 y1 ... ; y2 y2 ...]
+    yMod = kron(reshape(signal,4,NumberofBits/2),ones(1,4)); % [y1 y1 ... ; y2 y2 ...]
     yMod(2,:) = conj(yMod(2,:)); % [y1 y1 ... ; y2* y2*...]
 
     % forming the equalization matrix
     hEq = zeros(4,NumberofBits);
-    hEq(:,[1:2:end]) = reshape(h,4,NumberofBits/2); % [h1 0 ... ; h2 0...]
-    hEq(:,[2:2:end]) = kron(ones(2,NumberofBits/2),[1;-1]).*flipud(reshape(h,4,NumberofBits/2)); % [h1 h2 ... ; h2 -h1 ...]
+    hEq(:,[1:2:end]) = reshape(rayleigh,4,NumberofBits/2); % [h1 0 ... ; h2 0...]
+    hEq(:,[2:2:end]) = kron(ones(2,NumberofBits/2),[1;-1]).*flipud(reshape(rayleigh,4,NumberofBits/2)); % [h1 h2 ... ; h2 -h1 ...]
     hEq(1,:) = conj(hEq(1,:)); %  [h1* h2* ... ; h2 -h1 .... ]
     hEqPower = sum(hEq.*conj(hEq),1);
     yHat = sum(hEq.*yMod,1)./hEqPower; % [h1*y1 + h2y2*, h2*y1 -h1y2*, ... ]
@@ -46,7 +46,7 @@ for ii = 1:length(Eb_N0_dB)
     ipHat = real(yHat) > 0;
 
     % counting the errors
-    nErr(ii) = size(find([ip- ipHat]),2);
+    nErr(index) = size(find([ip- ipHat]),2);
 end
 
 simBer = nErr/NumberofBits; % simulated ber
