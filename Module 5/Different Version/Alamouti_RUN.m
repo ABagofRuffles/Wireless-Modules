c8614e7a1c719981ff11e9a067f4045f04266e22
mstotter <mstotter@users.noreply.github.com>
2019-04-10 14:30:55 -0400

    Added some jpegs

--- Module 5/Different Version/Alamouti_RUN.m
+++ Module 5/Different Version/Alamouti_RUN.m
@@ -5,13 +5,19 @@
 
 ber = [];
 BER = [];
+ber4 = [];
 snr = [0:10];
 
 for i=0:10
     ber = [ber Alamoudi_2T1R(i,500)];        %Computing bit error rate for Alamoudi_2T1R
-%     BER = [BER Alamoudi_2T2R(i,500)];   %Computing bit error rate for Alamoudi_2T2R
+    BER = [BER Alamoudi_2T2R(i,500)];        %Computing bit error rate for Alamoudi_2T2R
+    %ber4 = [ber4 Alamouti_4T1R(i,500)];      %Computing bit error rate for Alamoudi_4T1R
 end
 
 semilogy(snr,ber,'--*g');
+title('Bit Error Rate (Rayleigh Channel)');
+xlabel('Eb/No, dB');
+ylabel('Bit Error Rate');
 hold on;
 semilogy(snr,BER,'--*c');
+legend('2Tx,1Rx','2Tx,2Rx');
